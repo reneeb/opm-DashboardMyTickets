b10b4e2943c985d49e3366c02c1e852c243d2b24
reneeb <reb@perl-services.de>
2015-11-26 10:53:32 +0100

    make changes for OTRS5

--- Kernel/Output/HTML/Dashboard/TicketMyTicketsDashlet.pm
+++ Kernel/Output/HTML/Dashboard/TicketMyTicketsDashlet.pm
@@ -1,12 +1,12 @@
 # --
-# Kernel/Output/HTML/DashboardTicketMyTicketsDashlet.pm
+# Kernel/Output/HTML/Dashboard/TicketMyTicketsDashlet.pm
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
 # did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
 # --
 
-package Kernel::Output::HTML::DashboardTicketMyTicketsDashlet;
+package Kernel::Output::HTML::Dashboard::TicketMyTicketsDashlet;
 
 use strict;
 use warnings;
